204de7735cf2fdf5e01011c31ae869ec7f22b90e
Ankit Rai <94431712+ankitkumarrai502@users.noreply.github.com>
2026-04-24 14:00:40 +0530

    Change database name to LQTest

--- LQbot.py
+++ LQbot.py
@@ -18,7 +18,7 @@ load_dotenv(override=True)
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 SERVER = '208.112.103.123'
-DATABASE = 'LearnQuest'
+DATABASE = 'LQTest'
 USERNAME = 'learnquest2'
 
 CONNECTION_STRING = f"mssql+pyodbc://{USERNAME}:{DB_PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
